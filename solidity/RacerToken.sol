9c95c77ef5050d24d7f7cb0a52aab9c279ea083d
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-02-11 20:09:52 -0800

    Smart Contracts FINAL

--- solidity/RacerToken.sol
+++ solidity/RacerToken.sol
@@ -5,38 +5,34 @@ pragma solidity ^0.8.17;
 contract RacerToken {
 
     //Events: -------------------------
-
-    event Mint (
-        address Minter,
-        uint256 Amount,
-        uint256 Payment,
-        uint256 Time
-    ); 
+    event Mint (address Minter, uint256 Amount, uint256 Payment, uint256 Time);
+    
+    event Transfer (address Reciever, uint256 Amount, uint256 Time);
+    event TransferFrom (address Sender, address Reciever, uint256 Amount, uint256 Time);   
 
     //Modifiers: ------------------------
-
-    modifier onlyOwner (address _caller) {
-        require (_caller == owner, "You are not the owner of this contract"); 
-        _;
-    }
-
     modifier onlyCaller (address _caller) {
         require (isCaller[_caller] == true, "You are not an authorized called"); 
         _; 
     }
 
     //State: ---------------------------------
-
-    address public owner; 
-    uint256 public price;
-    uint256 public supply = balanceOf[address(this)]; 
-
-    constructor (uint256 _price) {
-        owner = msg.sender; 
+    constructor (uint256 _price, address _racer) {
+        owner = msg.sender;
+        racer = _racer;  
         price = _price; 
+
+        isCaller[_racer] = true; 
+        isCaller[msg.sender] = true; 
+        isCaller[address(this)] = true; 
 
         balanceOf[address(this)] = 10 ** 18;  
     }
+
+    address public owner; 
+    address public racer;
+    address public racerUtils; 
+    uint256 public price;
 
     mapping (address => uint256) public balanceOf;
     mapping (address => bool) public isCaller; 
@@ -45,7 +41,7 @@ contract RacerToken {
 
     function mintRacer (uint256 _amount) public payable {
         require (msg.value == _amount * price, "Exact value expected"); 
-        require (supply >= _amount, "Insufficient Supply"); 
+        require (getSupply() >= _amount, "Insufficient Supply"); 
         
         balanceOf[msg.sender] += _amount;
         balanceOf[address(this)] -= _amount;
@@ -53,27 +49,39 @@ contract RacerToken {
         emit Mint (msg.sender, _amount, msg.value, block.timestamp);  
     }
 
-    function transfer (address _to, uint256 _amount) public onlyCaller (msg.sender) {
-        require (supply >= _amount, "Insufficient Supply"); 
+    //-------------------------------------------
+
+    function transfer (address _to, uint256 _amount) external onlyCaller (msg.sender) {
+        require (getSupply() >= _amount, "Insufficient Supply"); 
 
         balanceOf[_to] += _amount; 
         balanceOf[address(this)] -= _amount; 
+
+        emit Transfer (_to, _amount, block.timestamp); 
     }
 
-    function transferFrom (address _from, address _to, uint256 _amount) public onlyCaller (msg.sender) {
-        require(balanceOf[_from] >= _amount, "Insufficient balance"); 
+    function transferFrom (address _from, address _to, uint256 _amount) external onlyCaller (msg.sender) {
+        require(getUserBalance(_from) >= _amount, "Insufficient balance"); 
 
         balanceOf[_from] -= _amount; 
-        balanceOf[_to] += _amount; 
+        balanceOf[_to] += _amount;
+
+        emit TransferFrom (_from, _to, _amount, block.timestamp);  
+    }
+
+    //Getters: -------------------------
+
+    function getSupply () public view returns (uint256) {
+        return balanceOf[address(this)]; 
+    }
+
+    function getUserBalance (address _user) public view returns (uint256) {
+        return balanceOf[_user]; 
     }
 
     //Setters: ------------------------
 
-    function setPrice (uint256 _newPrice) external onlyOwner (msg.sender) {
-        price = _newPrice; 
-    }
-
-    function editSupply (uint256 _amount, bool _increase) external onlyOwner (msg.sender) {
+    function editSupply (uint256 _amount, bool _increase) external onlyCaller (msg.sender) {
 
         if (_increase == true) {
             balanceOf[address(this)] += _amount; 
@@ -83,30 +91,25 @@ contract RacerToken {
         }
     }
 
-    function editCallers (address[] memory _callers, bool _add) external onlyOwner (msg.sender) { 
-
-        for (uint i = 0; i <= _callers.length; i++) {
-            isCaller[_callers[i]] = _add; 
-        }
+    function setPrice (uint256 _newPrice) external onlyCaller (msg.sender) {
+        price = _newPrice; 
     }
 
-    function transferOwnership (address _newOwner) public onlyOwner (msg.sender) {
+    function setCallers (address _newCaller) public onlyCaller (msg.sender) { 
+        isCaller[_newCaller] = true; 
+    }
+
+    function setContracts (address _racer, address _racerUtils) external onlyCaller (msg.sender) {
+        racer = _racer;
+        racerUtils = _racerUtils;
+
+        setCallers(_racer);
+        setCallers(_racerUtils);  
+    }
+
+    function transferOwnership (address _newOwner) external onlyCaller (msg.sender) {
         owner = _newOwner; 
     }
 
-    //Getters: -------------------------
-
-    function getSupply () public view returns (uint256) {
-        return balanceOf[address(this)]; 
-    }
-
-    function getBalance () public view returns (uint256) {
-        return address(this).balance; 
-    }
-
-    //Withdrawl: ----------------------
-
-    function withdraw () public onlyOwner (msg.sender) {
-        payable(msg.sender).transfer(address(this).balance); 
-    }
+    
 }
